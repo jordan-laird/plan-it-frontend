8e185f8ac57f2b4f586e455f54a549b8bcc158d7
barbaradecares <barbaradecares@hotmail.com>
2018-12-06 11:51:31 -0600

    fixed myQuotes/myQuotesCustomer

--- src/filter_buttons.js
+++ src/filter_buttons.js
@@ -7,88 +7,87 @@ let venuesImg = document.createElement("img");
 let entertainmentImg = document.createElement("img");
 let staffingImg = document.createElement("img");
 let decorationImg = document.createElement("img");
-let myQuotesImg = document.createElement("img");
+let myQuotesCustomerImg = document.createElement("img");
 let selectedService;
 let filteredVendors;
 
 //=======
-const filterButtons = function () {
-    topDiv.className = "container";
-    rowDiv.className = "row";
-    homeImg.className = "col-xs-3 col-sm-1 mx-auto";
-    homeImg.id = "filter-img";
-    homeImg.src = "images/home.png";
-    photographyImg.className = "col-xs-3 col-sm-1 mx-auto";
-    photographyImg.id = "filter-img";
-    photographyImg.src = "images/photo-camera.png";
-    cateringImg.className = "col-xs-3 col-sm-1 mx-auto";
-    cateringImg.id = "filter-img";
-    cateringImg.src = "images/buffet.png";
-    venuesImg.className = "col-xs-3 col-sm-1 mx-auto";
-    venuesImg.id = "filter-img";
-    venuesImg.src = "images/placeholder.png";
-    staffingImg.className = "col-xs-3 col-sm-1 mx-auto";
-    staffingImg.id = "filter-img";
-    staffingImg.src = "images/staff.png";
-    decorationImg.className = "col-xs-3 col-sm-1 mx-auto";
-    decorationImg.id = "filter-img";
-    decorationImg.src = "images/floating-balloons.png";
-    entertainmentImg.className = "col-xs-3 col-sm-1 mx-auto";
-    entertainmentImg.id = "filter-img";
-    entertainmentImg.src = "images/drummer-set.png";
-    myQuotesImg.className = "col-xs-3 col-sm-1 mx-auto";
-    myQuotesImg.id = "filter-img"
-    myQuotesImg.src = "images/receipt.png"
+const filterButtons = function() {
+  topDiv.className = "container";
+  rowDiv.className = "row";
+  homeImg.className = "col-xs-3 col-sm-1 mx-auto";
+  homeImg.id = "filter-img";
+  homeImg.src = "images/home.png";
+  photographyImg.className = "col-xs-3 col-sm-1 mx-auto";
+  photographyImg.id = "filter-img";
+  photographyImg.src = "images/photo-camera.png";
+  cateringImg.className = "col-xs-3 col-sm-1 mx-auto";
+  cateringImg.id = "filter-img";
+  cateringImg.src = "images/buffet.png";
+  venuesImg.className = "col-xs-3 col-sm-1 mx-auto";
+  venuesImg.id = "filter-img";
+  venuesImg.src = "images/placeholder.png";
+  staffingImg.className = "col-xs-3 col-sm-1 mx-auto";
+  staffingImg.id = "filter-img";
+  staffingImg.src = "images/staff.png";
+  decorationImg.className = "col-xs-3 col-sm-1 mx-auto";
+  decorationImg.id = "filter-img";
+  decorationImg.src = "images/floating-balloons.png";
+  entertainmentImg.className = "col-xs-3 col-sm-1 mx-auto";
+  entertainmentImg.id = "filter-img";
+  entertainmentImg.src = "images/drummer-set.png";
+  myQuotesCustomerImg.className = "col-xs-3 col-sm-1 mx-auto";
+  myQuotesCustomerImg.id = "filter-img";
+  myQuotesCustomerImg.src = "images/receipt.png";
 
+  navDiv.append(topDiv);
+  topDiv.append(rowDiv);
+  rowDiv.append(
+    homeImg,
+    photographyImg,
+    cateringImg,
+    venuesImg,
+    entertainmentImg,
+    staffingImg,
+    decorationImg,
+    myQuotesCustomerImg
+  );
+};
 
-    navDiv.append(topDiv);
-    topDiv.append(rowDiv);
-    rowDiv.append(
-        homeImg,
-        photographyImg,
-        cateringImg,
-        venuesImg,
-        entertainmentImg,
-        staffingImg,
-        decorationImg,
-        myQuotesImg
-    );
-}
-
-homeImg.addEventListener("click", function () {
-    selectedService = null;
-    renderAvailableVendors();
+homeImg.addEventListener("click", function() {
+  selectedService = null;
+  renderAvailableVendors();
 });
-photographyImg.addEventListener("click", function () {
-    selectedService = "Photography";
-    renderAvailableVendors();
+photographyImg.addEventListener("click", function() {
+  selectedService = "Photography";
+  renderAvailableVendors();
 });
-cateringImg.addEventListener("click", function () {
-    selectedService = "Catering";
-    renderAvailableVendors();
+cateringImg.addEventListener("click", function() {
+  selectedService = "Catering";
+  renderAvailableVendors();
 });
-venuesImg.addEventListener("click", function () {
-    selectedService = "Venues";
-    renderAvailableVendors();
+venuesImg.addEventListener("click", function() {
+  selectedService = "Venues";
+  renderAvailableVendors();
 });
-staffingImg.addEventListener("click", function () {
-    selectedService = "Staffing";
-    renderAvailableVendors();
+staffingImg.addEventListener("click", function() {
+  selectedService = "Staffing";
+  renderAvailableVendors();
 });
-decorationImg.addEventListener("click", function () {
-    selectedService = "Decoration";
-    renderAvailableVendors();
+decorationImg.addEventListener("click", function() {
+  selectedService = "Decoration";
+  renderAvailableVendors();
 });
-entertainmentImg.addEventListener("click", function () {
-    selectedService = "Photography";
-    renderAvailableVendors();
+entertainmentImg.addEventListener("click", function() {
+  selectedService = "Photography";
+  renderAvailableVendors();
 });
-myQuotesImg.addEventListener("click", function () {
-    vendorsListedSection = "";
-    renderMyQuotes();
+myQuotesCustomerImg.addEventListener("click", function() {
+  vendorsListedSection = "";
+  renderMyQuotes();
 });
 
-const filterByService = function (selectedService) {
-    filteredService = vendors.filter(vendor => vendor.service == selectedService);
-    return filteredService;
+const filterByService = function(selectedService) {
+  filteredService = vendors.filter(vendor => vendor.service == selectedService);
+  return filteredService;
 };
